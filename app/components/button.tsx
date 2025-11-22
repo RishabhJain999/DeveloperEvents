7aeb1466fae0ac20ade66ae17be78461bb7893a8
Rishabh Jain <rijain@prepaynation.com>
2025-11-22 17:59:19 +0530

    refactor code

--- app/components/button.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-const Button = () => {
-   console.log("running in:", typeof window === "undefined" ? "server" : "client");
-   return <button>Button</button>;
-};
-
-export default Button;
